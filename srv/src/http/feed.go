c4692f72e3b0bf18f737add2bbd4e353274fe3ac
Brian Picciano <mediocregopher@gmail.com>
2022-05-20 17:51:41 -0600

    Add tag parameter to feed endpoint

--- srv/src/http/feed.go
+++ srv/src/http/feed.go
@@ -7,11 +7,30 @@ import (
 
 	"github.com/gorilla/feeds"
 	"github.com/mediocregopher/blog.mediocregopher.com/srv/http/apiutil"
+	"github.com/mediocregopher/blog.mediocregopher.com/srv/post"
 )
 
 func (a *api) renderFeedHandler() http.Handler {
 
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+		tag := r.FormValue("tag")
+
+		var (
+			posts []post.StoredPost
+			err   error
+		)
+
+		if tag == "" {
+			posts, _, err = a.params.PostStore.Get(0, 20)
+		} else {
+			posts, err = a.params.PostStore.GetByTag(tag)
+		}
+
+		if err != nil {
+			apiutil.InternalServerError(rw, r, fmt.Errorf("fetching recent posts: %w", err))
+			return
+		}
 
 		author := &feeds.Author{
 			Name: "mediocregopher",
@@ -26,13 +45,7 @@ func (a *api) renderFeedHandler() http.Handler {
 			Author:      author,
 		}
 
-		recentPosts, _, err := a.params.PostStore.Get(0, 20)
-		if err != nil {
-			apiutil.InternalServerError(rw, r, fmt.Errorf("fetching recent posts: %w", err))
-			return
-		}
-
-		for _, post := range recentPosts {
+		for _, post := range posts {
 
 			if post.PublishedAt.After(feed.Updated) {
 				feed.Updated = post.PublishedAt
